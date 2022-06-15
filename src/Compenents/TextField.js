29a7526a4ddf853a809f1c9c08c1a205b749d369
fahimahammed <fahimahammed.cse@gmail.com>
2022-06-15 18:06:13 +0600

    add text area and button image

--- src/Compenents/TextField.js
+++ src/Compenents/TextField.js
@@ -4,8 +4,12 @@ import { useSpeechSynthesis } from 'react-speech-kit';
 const TextField = () => {
     const { speak } = useSpeechSynthesis();
     return (
-        <div>
-            <button className="btn btn-primary" onClick={() => speak({text: "Fahim Ahammed Firoz"})}>Click</button>
+        <div className='my-16'>
+            <textarea type="text" className="shadow appearance-none border rounded w-[450px] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+            <br/>
+            <button className="" onClick={() => speak({text: "Fahim Ahammed Firoz"})}>
+                <img className='w-16' src="https://icon-library.com/images/transparent-speaker-icon/transparent-speaker-icon-11.jpg" alt="Speak"></img>
+            </button>
         </div>
     );
 };
