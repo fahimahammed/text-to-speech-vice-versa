5d8623233d4c44b08e94cb3e09dcd42978576e60
fahimahammed <fahimahammed.cse@gmail.com>
2022-06-15 17:01:44 +0600

    speak test done

--- src/Compenents/TextField.js
+++ src/Compenents/TextField.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { useSpeechSynthesis } from 'react-speech-kit';
 
 const TextField = () => {
-    
+    const { speak } = useSpeechSynthesis();
     return (
         <div>
-            
+            <button className="btn btn-primary" onClick={() => speak({text: "Fahim Ahammed Firoz"})}>Click</button>
         </div>
     );
 };
